c2015c5d1722567333c66454a4bec908b40056cb
Scott Williams <williamsjs1224@gmail.com>
2015-05-07 13:19:56 -0400

    Testing to validate file linkage

---
+++ currency_program.rb
@@ -0,0 +1,8 @@
+require './currency_converter.rb'
+require './currency.rb'
+
+my_currency = Currency.new # creates instance of currency class
+converter = CurrencyConverter.new #creates instance of currencyconverter class
+
+puts my_currency
+puts converter
