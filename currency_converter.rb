4fb833615e04a9fbcd10196ac1751933f4c11f3f
Scott Williams <williamsjs1224@gmail.com>
2015-05-07 15:17:35 -0400

    create a simple hash inside of class to contain world currencies

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,3 +1,11 @@
 class CurrencyConverter
+  def initialize
+    world_currencies = {USD: 1.0,
+                        EUR: 0.89}
+  end
+
+  def convert(currency_object, currency_code)
+
+  end
 
 end
